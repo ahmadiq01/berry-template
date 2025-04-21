63dde06eb3ff3a70bd31089c1ba301d0706171e5
Ahmad Qureshi <89626382+ahmadiq01@users.noreply.github.com>
2025-04-21 17:19:44 +0500

    Initial commit

---
+++ src/routes/MainRoutes.jsx
@@ -0,0 +1,54 @@
+import { lazy } from 'react';
+import { Navigate } from 'react-router-dom';
+import MainLayout from 'layout/MainLayout';
+import Loadable from 'ui-component/Loadable';
+
+// Dashboard routing
+const DashboardDefault = Loadable(lazy(() => import('views/dashboard/Default')));
+
+// Utility routing
+const UtilsTypography = Loadable(lazy(() => import('views/utilities/Typography')));
+const UtilsColor = Loadable(lazy(() => import('views/utilities/Color')));
+const UtilsShadow = Loadable(lazy(() => import('views/utilities/Shadow')));
+
+// Sample page routing
+const SamplePage = Loadable(lazy(() => import('views/sample-page')));
+
+// ==============================|| MAIN ROUTING ||============================== //
+const MainRoutes = {
+  path: '/',
+  element: <MainLayout />,
+  children: [
+    {
+      path: '/',
+      element: <Navigate to="/pages/login" /> // Default route to login
+    },
+    {
+      path: 'dashboard',
+      children: [
+        {
+          path: 'default',
+          element: <DashboardDefault />
+        }
+      ]
+    },
+    {
+      path: 'typography',
+      element: <UtilsTypography />
+    },
+    {
+      path: 'color',
+      element: <UtilsColor />
+    },
+    {
+      path: 'shadow',
+      element: <UtilsShadow />
+    },
+    {
+      path: '/sample-page',
+      element: <SamplePage />
+    }
+  ]
+};
+
+export default MainRoutes;
